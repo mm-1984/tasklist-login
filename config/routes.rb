29ce44d541a47c9cad3defeed08a0b7f6b8a0e67
ziro.yamada <www20991610@gmail.com>
2020-08-24 06:47:47 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
     root to: 'tasks#index'
     resources :tasks
+    
+    get 'signup', to: 'users#new'
+    resources :users, only: [:new, :create]
 end
