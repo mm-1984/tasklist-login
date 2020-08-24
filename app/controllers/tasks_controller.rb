32119a7b413416db41feb914833edc294533c744
ziro.yamada <www20991610@gmail.com>
2020-08-24 09:30:46 +0000

    list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,23 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:new, :show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy, :show, :edit, :update]
   
   def index
-      @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc)
+    end
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクが作成されました'
       redirect_to @task
@@ -39,7 +42,6 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    
     flash[:success] = 'task は正常に削除されました'
     redirect_to tasks_url
   end
@@ -52,5 +54,12 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
